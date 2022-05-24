c5bc8535ae82c0d60c3af94965da7f3c0f531859
Leandro Rafael Duran Minaya <LeandroDuran24@users.noreply.github.com>
2022-05-23 21:57:15 -0400

    Subiendo proyecto

---
+++ Sistema_Facturacion/Models/ConexionDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_Facturacion.Models
+{
+    public class ConexionDb
+    {
+
+        public static SqlConnection connectionString = new SqlConnection("Server=LEANDRODURAN\\SQLEXPRESS; DataBase=BarberShop; Integrated Security=true");
+
+
+        public SqlConnection AbrirConexion()
+        {
+
+            if (connectionString.State == System.Data.ConnectionState.Closed)
+                connectionString.Open();
+            return connectionString;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (connectionString.State == System.Data.ConnectionState.Open)
+                connectionString.Close();
+            return connectionString;
+        }
+    }
+}
---
+++ Sistema_Facturacion/Models/Usuarios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sistema_Facturacion.Models
+{
+    public class Usuarios
+    {
+
+
+        [DisplayName("Id Usuario ")]
+        public int IdUsuario { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellidos { get; set; }
+        [Required]
+        public string Usuario { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Clave { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+
+
+        public Usuarios()
+        {
+
+        }
+
+
+        public Usuarios(int IdUsuario)
+        {
+
+        }
+    }
+
+}
